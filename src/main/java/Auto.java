06ce40571a25c4c8a213e8eb2c5ac3998e9debf1
Roberta <Roberta@DESKTOP-V9BA4DQ.station>
2021-05-24 17:09:06 +0200

    Aggiunto Main, Auto, Autosalone Completata la classe Autosalone (1 errore) Javadoc finito

--- src/main/java/Auto.java
+++ src/main/java/Auto.java
@@ -8,7 +8,23 @@
 import java.time.LocalDate;
 
 /**
+ * 
+ * /**
+ * la classe rappresenta un Auto
+ * i suoi attributi sono:codice
+ * nome
+ * cognome
+ * marca
+ * modello
+ * targa
+ * vendita
+ * @version 1.0
  *
+ * @author Roberta
+ */
+
+/**
+ * 
  * @author Roberta
  */
 public class Auto 
@@ -21,6 +37,19 @@ public class Auto
         private String Targa;
         private LocalDate Vendita;
 
+        /**
+         * 
+         * @param Codice 
+         * @param Nome nome cliente
+         * @param Cognome cognome cliente
+         * @param Marca marca auto
+         * @param Modello modello auto
+         * @param Targa targa auto
+         * @param anno 
+         * @param mese
+         * @param giorno 
+         * Attraverso anno, mese, giorno si troverà l'auto venduta in det. giorno
+         */
          public Auto(int Codice, String Nome, String Cognome, String Marca, String Modello, String Targa,  int anno,int mese,int giorno)
          {
              this.Codice = Codice;
@@ -32,6 +61,12 @@ public class Auto
              this.Vendita=LocalDate.of(anno,mese,giorno);
              
          }
+         
+         /**
+          * 
+          * @param a Auto da cui sarà istanziata una nuova auto
+          * 
+          */
          
          public Auto (Auto a)
                  
@@ -47,37 +82,72 @@ public class Auto
           }
     
     
-
-         
+           
          public int getCodice() 
     {
         return Codice;
     }
 
+         /**
+          * 
+  
+          * @return 
+          * Assegna nome al cliente
+          */
     public String getnome() 
     {
         return nome;
     }
 
+    /**
+          * 
+  
+          * @return 
+          * Assegna cognome al cliente
+          */
     public String getcognome() 
     {
         return cognome;
     }
 
+    /**
+          * 
+  
+          * @return 
+          * Assegna marca all auto
+          */
     public String getMarca() 
     {
         return Marca;
     }
+    
+    /**
+          * 
+  
+          * @return 
+          * Assegna modello all auto
+          */
     public String getModello() 
     {
         return Modello;
     }
     
+    /**
+          * 
+  
+          * @return 
+          * Assegna targa all'auto
+          */
     public String getTarga() 
     {
         return Targa;
     }
-    
+    /**
+          * 
+  
+          * @return 
+          * Assegna vendita al cliente
+          */
 
    public LocalDate getVendita() 
     {
@@ -90,34 +160,67 @@ public class Auto
         this.Codice = Codice;
     }
 
+    /**
+     * 
+     * @param Nome restituisce il nome
+     */
     public void setNome(String Nome) 
     {
         this.nome = Nome;
     }
 
+    /**
+     * 
+     * @param Cognome restituisce il cognome
+     */
     public void setCognome(String Cognome) 
     {
         this.cognome = Cognome;
     }
 
+    /**
+     * 
+     * @param Marca restituisce la marca
+     */
     public void setMarca(String Marca) 
     {
         this.Marca = Marca;
     }
+    
+    /**
+     * 
+     * @param Modello restituisce il modello
+     */
+    
      public void setModello(String Modello) 
     {
         this.Marca = Modello;
     }
+     
+     /**
+     * 
+     * @param Targa restituisce la targa
+     */
+     
       public void setTarga(String Targa) 
     {
         this.Targa = Targa;
     }
     
+      /**
+       * 
+       * @param Vendita restituisce la vendita
+       */
 
     public void setVendita(LocalDate Vendita) 
     {
         this.Vendita=Vendita;
     }
+    
+   /**
+     * restituisce una stringa contenente tutti gli attributi di un auto
+     * @return s stringa contente attributi dell auto
+     */ 
 
   public String toString()
    {
@@ -125,4 +228,12 @@ public class Auto
        s="Codice: "+getCodice()+"; Nome: "+getnome()+"; Cognome: "+getcognome()+"; Marca: "+getMarca()+"; Modello: "+getModello()+"; Targa: "+getTarga()+ " Vendita: "+getVendita();
        return s;
    }
+
+    void setVendita(int giorno, int mese, int anno) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    boolean Prezzo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
